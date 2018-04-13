f7ef4fc170c809d35277bd266b0729e5dec2e65b
Jassu <jasmine.lindelof@helsinki.fi>
2018-04-13 20:25:51 +0300

    Välitallennus

--- application/views.py
+++ application/views.py
@@ -1,6 +1,10 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.tasks.models import Task
+from application.auth.models import User
 
+@app.route('/')
+def index():
+    return render_template("index.html", needs_tasks=User.find_users_with_no_tasks())
 
 
