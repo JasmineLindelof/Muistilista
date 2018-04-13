24ba8f32a3402f776a360aa451ad88f847ae2c05
Jassu <jasmine.lindelof@helsinki.fi>
2018-04-13 20:45:25 +0300

    Ulkoasun muokkausta

--- application/auth/models.py
+++ application/auth/models.py
@@ -30,12 +30,12 @@ class User(Base):
 
 
     @staticmethod
-    def find_users_with_no_tasks():
+    def find_users_with_no_tasks(done=0):
         stmt = text("SELECT Account.id, Account.name FROM Account"
                      " LEFT JOIN Task ON Task.account_id = Account.id"
-                     " WHERE (Task.done IS null OR Task.done = 1)"
+                     " WHERE (Task.done IS null OR Task.done = :done)"
                      " GROUP BY Account.id"
-                     " HAVING COUNT(Task.id) = 0")
+                     " HAVING COUNT(Task.id) = 0").params(done=done)
         res = db.engine.execute(stmt)
 
         response = []
