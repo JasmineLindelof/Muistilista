b6b55c2f194467c5180f754783d850021a421af8
Jassu <jasmine.lindelof@helsinki.fi>
2018-05-11 01:55:04 +0300

    Ulkoasun viilausta ja jotain pientä toiminnallisuuttakin

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -3,10 +3,14 @@ from wtforms import BooleanField, SelectField,  StringField, validators
 
 class TaskForm(FlaskForm):
     name = StringField("Tehtävä", [validators.Length(min=2)])
-    category = SelectField("Kategoria", choices = [('1', 'Työ'),('2', 'Koti')])
+    category = SelectField(u'Group', coerce=int)
     #done = BooleanField("Tehty")
     importance = SelectField("Tärkeys", choices = [('1','1'),('2','2'),('3','3'),('4','4'),('5','5')])
   
     class Meta:
         csrf = False
 
+#def edit_user(request, id):
+#    user = User.query.get(id)
+#    form.group_id.choices = [(g.id, g.name) for g in categories('name')]
+#    form = UserDetails(request.POST, obj=user)
